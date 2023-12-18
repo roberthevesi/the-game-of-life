843e29c0858a09a0e4791e1aa4f9993ca0e1a0d8
Robert Hevesi <roberthevesi09@gmail.com>
2023-12-18 19:51:36 +0200

    Fixed EventConsumer

--- the-game-of-life/src/main/java/EventConsumer.java
+++ the-game-of-life/src/main/java/EventConsumer.java
@@ -15,15 +15,15 @@ public class EventConsumer implements Runnable {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(HOST); // Change if RabbitMQ is on a different host
         factory.setPort(PORT);
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+        try{
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
             DeliverCallback deliverCallback = (consumerTag, delivery) -> {
                 String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
                 System.out.println(" [x] Received '" + message + "'");
-                System.out.println("HELLO @@@@@@@@@@@@@@@@@@@@@@");
             };
             channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {});
         } catch (Exception e) {
--- the-game-of-life/src/main/java/EventPublisher.java
+++ the-game-of-life/src/main/java/EventPublisher.java
@@ -2,6 +2,9 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class EventPublisher {
     private final static String QUEUE_NAME = "cell_events";
     private final static String HOST = "localhost";
@@ -11,11 +14,14 @@ public class EventPublisher {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(HOST); // Change if RabbitMQ is on a different host
         factory.setPort(PORT);
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
             System.out.println(" [x] Sent '" + message + "'");
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException(e);
         }
     }
 }
